e096d4add3d4034275e69417f20e45d2b69a0dca
Charlie Brennan <charliebrennan50@gmail.com>
2020-11-06 15:16:00 -0500

    added random topic generator

--- public/sketch.js
+++ public/sketch.js
@@ -1,27 +1,45 @@
-async function getData() {
+import randomTopics from './random-topics.js';
 
-    //event.preventDefault();
+//async function getData() {
+document.getElementById('myBtn').addEventListener("click", async function (event) {
+    event.preventDefault();
     const searchTerm = document.getElementById("searchTerm").value;
     const searchLimit = "50";
     searchTerm.replace(/\s+/g, '_');
     let newURL = `https://en.wikipedia.org/w/rest.php/v1/search/page?q=${searchTerm}&limit=${searchLimit}`;
 
-    // let searchURL =
-    //     `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${searchTerm}`;
-    // let contentURL =
-    //     'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=';
-
     let response = await fetch(newURL);
     let data = await response.json();
-    console.log(data.pages);
 
-    randArticle = Math.floor(Math.random() * data.pages.length);
-    console.log(data.pages[randArticle].key);
-    console.log(data.pages[randArticle].title);
-    console.log(data.pages[randArticle].description);
-    console.log(data.pages[randArticle].excerpt);
+    let randArticle = Math.floor(Math.random() * data.pages.length);
+
+    let pageURL = `http://en.wikipedia.org/?curid=${data.pages[randArticle].id}`
+    document.getElementById("page-URL").href = pageURL;
+    document.getElementById("page-URL").classList.add("active");
 
     document.getElementById("article-title").innerHTML = data.pages[randArticle].title;
     document.getElementById("article-excerpt").innerHTML = data.pages[randArticle].excerpt;
 
-}
+})
+
+document.getElementById('randBtn').addEventListener("click", async function (event) {
+    event.preventDefault();
+    const searchTerm = randomTopics[Math.floor(Math.random() * randomTopics.length)];
+    document.getElementById("searchTerm").value = searchTerm;
+    const searchLimit = "50";
+    searchTerm.replace(/\s+/g, '_');
+    let newURL = `https://en.wikipedia.org/w/rest.php/v1/search/page?q=${searchTerm}&limit=${searchLimit}`;
+
+    let response = await fetch(newURL);
+    let data = await response.json();
+
+    let randArticle = Math.floor(Math.random() * data.pages.length);
+
+    let pageURL = `http://en.wikipedia.org/?curid=${data.pages[randArticle].id}`
+    document.getElementById("page-URL").href = pageURL;
+    document.getElementById("page-URL").classList.add("active");
+
+    document.getElementById("article-title").innerHTML = data.pages[randArticle].title;
+    document.getElementById("article-excerpt").innerHTML = data.pages[randArticle].excerpt;
+
+})
